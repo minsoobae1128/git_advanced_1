c2bec0692d467d5a1788713a825cb3084279cbc6
minsoobae1128 <iaianiqa@gmail.com>
2025-04-30 18:39:17 +0900

    Add skeleton code to main branch

---
+++ main.py
@@ -0,0 +1,26 @@
+from typing import List
+
+# Skeleton code for even_list
+def even_list(int_list: List[int]) -> List[int]:
+    """
+    Determines if a number is even and return an even list.
+    """
+    # TODO: Implement even_list
+    pass
+
+# Skeleton code for sum_of_squares_of_even
+def sum_of_squares_of_even(even_int_list: List[int]) -> int:
+    """
+    Computes the sum of the squares of all even numbers in a list of integers.
+    """
+    # TODO: Implement sum_of_squares_of_even
+    pass
+
+def main():
+    int_list = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+    even_int_list = even_list(int_list)
+    output = sum_of_squares_of_even(even_int_list)
+    print(output)
+
+if __name__ == "__main__":
+    main()
